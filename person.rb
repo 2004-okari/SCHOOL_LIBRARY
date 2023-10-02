4ed2e8c55f5cb01fa324c6aceff11779a63ef3bb
okari <nyandika15@gmail.com>
2023-10-02 16:02:08 +0300

    Add files

---
+++ person.rb
@@ -0,0 +1,40 @@
+class Person
+  attr_reader :id, :name, :age
+
+  def initialize(id, name= 'unknown', age, parent_permission = true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def age=(new+age)
+    @age = new_age
+  end
+  
+  private
+  
+  def of_age?
+    @age >= 18
+  end
+  
+    def can_use_services?
+      of_age? || @parent_permission
+    end
+end
