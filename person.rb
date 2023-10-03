8048df7aca1e88e6222030f9109a9ba665f79d21
okari <nyandika15@gmail.com>
2023-10-03 15:39:35 +0300

    Linter fix

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(id, age, name: 'unknown', parent_permission: true)
+  def initialize(_id, age, name: 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
